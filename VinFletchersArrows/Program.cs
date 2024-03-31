70e24ab1ada779798d2b88e42fce1e0c10349149
ckumler <corey.kumler@gmail.com>
2024-03-31 12:18:23 -0400

    Competed "Ch21 - Arrow Factories"
    Implement Elite, Beginner and Marksman Arrow Factories.
    Update program to allow user to select premade or custom made arrows

--- VinFletchersArrows/Program.cs
+++ VinFletchersArrows/Program.cs
@@ -1,33 +1,63 @@
 ﻿ArrowHeadType myArrowHeadType;
 ArrowFletchingType myArrowFletchingType;
 int myArrowLength;
+Arrow myArrow = null;
 
 Console.WriteLine("Welcome to Vin Fletcher's Arrow Shop!\n");
-Console.Write("What type of arrowhead would you like? (Steel, Obsidian, Wool)");
-while (!Enum.TryParse<ArrowHeadType>(Console.ReadLine(), out myArrowHeadType)) {
+
+Console.WriteLine("Would you like to order a premade arrow or customize your own? (Premade, Custom)");
+OrderType myOrderType;
+while (!Enum.TryParse<OrderType>(Console.ReadLine(), out myOrderType)) {
     WriteErrorText("Invalid input. Please try again.");
-    Console.Write("What type of arrowhead would you like? (Steel, Obsidian, Wool)");
+    Console.WriteLine("Would you like to order a premade arrow or customize your own? (Premade, Custom)");
 }
 Console.WriteLine();
-Console.Write("What type of fletching would you like? (Plastic, TurkeyFeather, GooseFeather)");
-while (!Enum.TryParse<ArrowFletchingType>(Console.ReadLine(), out myArrowFletchingType)) {
-    WriteErrorText("Invalid input. Please try again.");
+
+if (myOrderType == OrderType.Premade) {
+    Console.Write("What type of premade arrow would you like? (Beginner, Marksman, Elite)");
+    PremadeArrowType myPremadeArrowType;
+    while (!Enum.TryParse<PremadeArrowType>(Console.ReadLine(), out myPremadeArrowType)) {
+        WriteErrorText("Invalid input. Please try again.");
+        Console.Write("What type of premade arrow would you like? (Beginner, Marksman, Elite)");
+    }
+    Console.WriteLine();
+
+    switch (myPremadeArrowType) {
+        case PremadeArrowType.Beginner:
+            myArrow = Arrow.CreateBeginnerArrow();
+            break;
+        case PremadeArrowType.Marksman:
+            myArrow = Arrow.CreateMarksmanArrow();
+            break;
+        case PremadeArrowType.Elite:
+            myArrow = Arrow.CreateEliteArrow();
+            break;
+    }    
+} else {
+    Console.Write("What type of arrowhead would you like? (Steel, Obsidian, Wood)");
+    while (!Enum.TryParse<ArrowHeadType>(Console.ReadLine(), out myArrowHeadType)) {
+        WriteErrorText("Invalid input. Please try again.");
+        Console.Write("What type of arrowhead would you like? (Steel, Obsidian, Wood)");
+    }
+    Console.WriteLine();
     Console.Write("What type of fletching would you like? (Plastic, TurkeyFeather, GooseFeather)");
-}
-Console.WriteLine();
-Console.Write("What length would you like the arrow to be? (60-100 cm)");
-while (!int.TryParse(Console.ReadLine(), out myArrowLength) || myArrowLength < 60 || myArrowLength > 100) {
-    WriteErrorText("Invalid input. Please try again.");
+    while (!Enum.TryParse<ArrowFletchingType>(Console.ReadLine(), out myArrowFletchingType)) {
+        WriteErrorText("Invalid input. Please try again.");
+        Console.Write("What type of fletching would you like? (Plastic, TurkeyFeather, GooseFeather)");
+    }
+    Console.WriteLine();
     Console.Write("What length would you like the arrow to be? (60-100 cm)");
+    while (!int.TryParse(Console.ReadLine(), out myArrowLength) || myArrowLength < 60 || myArrowLength > 100) {
+        WriteErrorText("Invalid input. Please try again.");
+        Console.Write("What length would you like the arrow to be? (60-100 cm)");
+    }
+    Console.WriteLine();
+
+    myArrow = new Arrow(myArrowHeadType, myArrowFletchingType, myArrowLength);
 }
-Console.WriteLine();
 
-Arrow myArrow = new Arrow(myArrowHeadType, myArrowFletchingType, myArrowLength);
-Console.WriteLine($"You have ordered an arrow with a {myArrowHeadType} arrowhead, {myArrowFletchingType} fletching, and a length of {myArrowLength} cm.");
+Console.WriteLine($"You have ordered an arrow with a {myArrow.ArrowHeadType} arrowhead, {myArrow.ArrowFletchingType} fletching, and a length of {myArrow.ArrowLength} cm.");
 Console.WriteLine($"The cost of this arrow is {myArrow.GetCost()} gold.");
-
-
-
 
 void WriteErrorText(string text) {
     Console.ForegroundColor = ConsoleColor.Red;
@@ -50,6 +80,18 @@ class Arrow {
         ArrowLength = arrowLength;
     }
 
+    public static Arrow CreateBeginnerArrow() {
+        return new Arrow(ArrowHeadType.Wood, ArrowFletchingType.GooseFeather, 75);
+    }
+
+    public static Arrow CreateMarksmanArrow() {
+        return new Arrow(ArrowHeadType.Steel, ArrowFletchingType.GooseFeather, 65);
+    }
+
+    public static Arrow CreateEliteArrow() {
+        return new Arrow(ArrowHeadType.Steel, ArrowFletchingType.Plastic, 95);
+    }
+
     public float GetCost() {
         float cost = 0.05f * ArrowLength;
         switch (ArrowHeadType) {
@@ -59,7 +101,7 @@ class Arrow {
             case ArrowHeadType.Obsidian:
                 cost += 5;
                 break;
-            case ArrowHeadType.Wool:
+            case ArrowHeadType.Wood:
                 cost += 3;
                 break;
         }
@@ -78,5 +120,7 @@ class Arrow {
     }
 }
 
-enum ArrowHeadType { Steel, Obsidian, Wool };
+enum ArrowHeadType { Steel, Obsidian, Wood };
 enum ArrowFletchingType { Plastic, TurkeyFeather, GooseFeather };
+enum OrderType { Premade, Custom };
+enum PremadeArrowType { Beginner, Marksman, Elite };
