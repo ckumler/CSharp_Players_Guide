a58ef09f3464d0bd4b4d6d18f542b23c8bd2f72d
ckumler <corey.kumler@gmail.com>
2024-04-10 18:28:28 -0400

    Completed The Point

---
+++ ThePoint/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program {
+    public static void Main() {
+        Point[] points = [new Point(2, 3), new Point(-4, 0)];
+
+        foreach (Point p in points) {
+            Console.WriteLine($"({p.X},{p.Y})");
+        }
+    }
+}
+
+public class Point {
+    public int X { get; init; }
+    public int Y { get; init; }
+
+    public Point(int x , int y) {
+        X = x;
+        Y = y;
+    }
+
+    public Point() {
+        X = 0;
+        Y = 0;
+    }
+}
+
+
