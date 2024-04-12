33ba763532bf0c98243ce6209a02b22e293fbc0b
ckumler <corey.kumler@gmail.com>
2024-04-11 23:41:03 -0400

    Completed Labeling Inventory

--- PackingInventory/Program.cs
+++ PackingInventory/Program.cs
@@ -7,6 +7,15 @@ public class Game
     {
         bool running = true;
         int selection;
+
+        Pack shop = new Pack(100, 100, 100);
+        shop.Add(new Arrow());
+        shop.Add(new Bow());
+        shop.Add(new Rope());
+        shop.Add(new Water());
+        shop.Add(new Ration());
+        shop.Add(new Sword());
+
         Pack pack = new Pack(15, 9f, 12f);
 
         do
@@ -16,7 +25,7 @@ public class Game
             Console.WriteLine("* Select an item to add to your pack! (1 through 6, or type exit)");
             Console.WriteLine("");
             Console.ResetColor();
-            DisplayShop();
+            DisplayShop(shop);
             DisplayPack(pack);
             Console.Write("Select an item to add to your pack : ");
 
@@ -55,13 +64,10 @@ public class Game
             {
                 Console.WriteLine("Invalid input. Please enter a number from 1 to 6 or type 'exit' to leave.");
             }
-
         } while (running);
-
-
     }
 
-    private void DisplayShop()
+    private void DisplayShop(Pack pack)
     {
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine($@"|--------------------------------------|");
@@ -69,12 +75,12 @@ public class Game
         Console.WriteLine($@"|--------------------------------------|");
         Console.WriteLine($@"| Num | Item     | Weight   | Volume   |");
         Console.WriteLine($@"|-----+----------+----------+----------|");
-        Console.WriteLine($@"| 1   | Arrow    | 0.1      | 0.05     |");
-        Console.WriteLine($@"| 2   | Bow      | 1        | 4        |");
-        Console.WriteLine($@"| 3   | Rope     | 1        | 1.5      |");
-        Console.WriteLine($@"| 4   | Water    | 2        | 3        |");
-        Console.WriteLine($@"| 5   | Food     | 1        | 0.5      |");
-        Console.WriteLine($@"| 6   | Sword    | 5        | 3        |");
+        int iter = 0;
+        foreach (InventoryItem item in pack.Items)
+        {
+            iter++;
+            Console.WriteLine($@"| {iter,-3} | {item,-8} | {item.weight,-8} | {item.volume,-8} |");
+        }
         Console.WriteLine($@"|--------------------------------------|");
         Console.WriteLine("");
         Console.ResetColor();
@@ -93,7 +99,7 @@ public class Game
         foreach (InventoryItem item in pack.Items)
         {
             iter++;
-            Console.WriteLine($@"| {iter,-3} | {item.GetType(),-8} | {item.weight,-8} | {item.volume,-8} |");
+            Console.WriteLine($@"| {iter,-3} | {item,-8} | {item.weight,-8} | {item.volume,-8} |");
         }
 
         Console.WriteLine($@"|--------------------------------------|");
@@ -119,6 +125,11 @@ public class InventoryItem
     {
         this.weight = weight;
         this.volume = volume;
+    }
+
+    public override string ToString()
+    {
+        return this.GetType().Name;
     }
 }
 
